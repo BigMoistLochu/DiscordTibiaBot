e25a5dbfb5b1d012a214fa109ac0b3b192e7cee9
BigMoistLochu <krok.konrad1@gmail.com>
2023-12-04 22:16:06 +0100

    add class model which will represent a Character from website, and Thread which will scrapp this data from webiste

---
+++ src/main/java/com/example/discordtibiabot/events/GetInfoCharacter.java
@@ -0,0 +1,16 @@
+package com.example.discordtibiabot.events;
+
+
+
+public class GetInfoCharacter implements Runnable{
+
+
+    @Override
+    public void run() {
+        //wchodzi na strone i pobiera dane uzytkownika
+        //1. zaimplementuj jsoup library
+        //2.metoda ma zwracac obiekt Character ktora posiada nick,lvl,exp
+        //3.mozna dodac do discorda albo zwrocenie kolejnej postaci albo
+
+    }
+}
---
+++ src/main/java/com/example/discordtibiabot/models/Character.java
@@ -0,0 +1,9 @@
+package com.example.discordtibiabot.models;
+
+public class Character {
+
+    //klasa character ktora bedzie definiowac obiekt naszej postaci ktora ma sledzic
+
+
+
+}
